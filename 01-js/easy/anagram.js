1326dba5b14d239764880ce71e950f1e5e92ed2a
Samyak Shah <samyakshah3008@gmail.com>
2024-03-13 01:39:39 +0530

    add: anagram solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
+  // remove whitespaces and convert to lowercase
+  // count number of characters for both str1 and str2
+  // convert the strings to arrays, sort them, and return back to string
 
+  str1 = str1.replace(/\s/g, "").toLowerCase();
+  str2 = str2.replace(/\s/g, "").toLowerCase();
+
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const sortedStr1 = str1.split("").sort().join("");
+  const sortedStr2 = str2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
 }
+
+isAnagram("debit card%", "%bad credit");
 
 module.exports = isAnagram;
