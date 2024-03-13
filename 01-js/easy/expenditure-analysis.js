0fe6894e3bdce672ab2131386b9b2cd2b0aec968
Samyak Shah <samyakshah3008@gmail.com>
2024-03-13 23:32:05 +0530

    add: expenditure analysis sol

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -13,8 +13,89 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+// const transactionsList = [
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: "Food",
+//     itemName: "Pizza",
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1656076800000,
+//     price: 100,
+//     category: "Food",
+//     itemName: "Pizza",
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1656076800000,
+//     price: 1000,
+//     category: "Book",
+//     itemName: "Atomic Habits",
+//   },
+// ];
+
+// const calculateTotalSpentByCategory = (transactionsList) => {
+//   const getAllCategories = transactionsList.map((eachTransaction) => {
+//     return eachTransaction.category;
+//   });
+
+//   const getAllUniqueCategories = Array.from(new Set(getAllCategories));
+
+//   const finalList = getAllUniqueCategories.map((eachCategory) => {});
+// };
+
+// calculateTotalSpentByCategory(transactionsList)
+
+function calculateTotalSpentByCategory(arr) {
+  let outputArr = [];
+  arr.forEach((element) => {
+    if (outputArr.length >= 1) {
+      outputArr.forEach((elem) => {
+        if (element.category === elem.category) {
+          elem.totalSpent += element.price;
+        } else {
+          outputArr.push({
+            category: element.category,
+            totalSpent: element.price,
+          });
+        }
+      });
+    } else {
+      outputArr.push({
+        category: element.category,
+        totalSpent: element.price,
+      });
+    }
+  });
+  return outputArr;
 }
+
+let output = calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Shoes",
+    itemName: "Pizza",
+  },
+]);
+console.log(output);
 
 module.exports = calculateTotalSpentByCategory;
